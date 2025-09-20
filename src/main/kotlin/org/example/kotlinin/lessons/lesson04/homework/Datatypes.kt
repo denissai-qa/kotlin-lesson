acef606c4003cea3263f5614ef8dd450cc2a3af4
Denis <denis93sai@gmail.com>
2025-09-20 14:48:19 +0300

    Datatypes Homework # 4

---
+++ src/main/kotlin/org/example/kotlinin/lessons/lesson04/homework/Datatypes.kt
@@ -0,0 +1,47 @@
+package org.example.org.example.kotlinin.lessons.lesson04.homework
+
+val v1 : Byte = 42
+val v2 : Long = 98765432123456789L
+val v3 : Float = 23.45f
+val v4 : Double = 0.123456789
+val v5 : String = "Kotlin & Java"
+// ощибка val v6 : Boolean = FALSE
+val v7 : Char = 'c'
+val v8 : Byte = 500
+val v9 : Long = 4294967296L
+val v10 : Float = 18.0f
+val v11 : Double = -0.001
+val v12 : String = "OpenAI"
+val v13 : String = "true"
+val v14 : List <Int> = listOf(3, 14)
+val v15 : Char = '9'
+val v16 : Int =  2048
+val v17 : Long =  10000000000L
+val v18 = setOf("OpenAI", "Quantum Computing")
+val v19 = 5.75f
+// ошибка val v20 = `1.414`
+val v21 : String =  "Artificial Intelligence"
+val v22 : Array<Any> = arrayOf('x', "A")
+val v23 : String =  "Android Studio"
+val v24 : Char = '@'
+val v25 : Int = 1024
+val v26 : Long = 1234567890123L
+val v27 : Float = 10.01f
+val v28 : Double = -273.15
+val v29 : String = "SpaceX"
+// ошибка val v30 = FALSE
+val v31 : Double = 0.007
+// ошибка val v32 = “🤯”
+val v33 : Map<String, Byte> = mapOf("true" to 2, "false" to 34)
+// ошибка val v34 : = ‘65535’
+val v35 : Long = 72057594037927935L
+val v36 : Float =  2.71828f
+val v37 : Double = 101.0101
+val v38 : String = "Quantum Computing"
+val v39 : Map<Int, String> = mapOf(2 to "true", 34 to "false")
+val v40 : Char = 'x'
+val v41 : Int =  314
+val v42 : Long = 123456789123456789L
+val v43 : Float = 6.626f
+// ошибка val v44 = TRUE
+
